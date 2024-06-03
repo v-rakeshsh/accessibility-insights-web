f5e70811eae49d643401c571361c93e4b882155a
Saanica Ghate <v-sghate@microsoft.com>
2024-06-03 16:44:10 +0530

    migrated expand-collapse-all-button to v9, fixed css issues.

--- src/common/components/cards/expand-collapse-all-button.tsx
+++ src/common/components/cards/expand-collapse-all-button.tsx
@@ -1,11 +1,11 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
-import { Button } from '@fluentui/react-components'
+import { Button } from '@fluentui/react-components';
+import { ChevronDownRegular, ChevronRightRegular } from '@fluentui/react-icons';
 import { CardSelectionMessageCreator } from 'common/message-creators/card-selection-message-creator';
 import { NamedFC } from 'common/react/named-fc';
 import * as React from 'react';
 import styles from './expand-collapse-all-button.scss';
-import { ChevronDownRegular, ChevronRightRegular } from '@fluentui/react-icons';
 
 export type ExpandCollapseAllButtonProps = {
     allCardsCollapsed: boolean;
@@ -19,18 +19,19 @@ export const ExpandCollapseAllButton = NamedFC<ExpandCollapseAllButtonProps>(
 
         let expandCollapseAllButtonHandler = cardSelectionMessageCreator.collapseAllRules;
         let buttonText = 'Collapse all';
-        let iconName = <ChevronDownRegular/>;
+        let iconName = <ChevronDownRegular className={styles.chevronDownRegular} />;
         let ariaLabel: string | undefined = undefined;
 
         if (allCardsCollapsed) {
             expandCollapseAllButtonHandler = cardSelectionMessageCreator.expandAllRules;
             buttonText = 'Expand all';
-            iconName = <ChevronRightRegular/>;
+            iconName = <ChevronRightRegular className={styles.chevronRightRegular} />;
             ariaLabel = 'Expand all rules to show failed instances.';
         }
 
         return (
-            <Button appearance="transparent"
+            <Button
+                appearance="transparent"
                 icon={iconName}
                 onClick={expandCollapseAllButtonHandler}
                 aria-expanded={!allCardsCollapsed}
